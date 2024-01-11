cc10f4c990f550b21d7aee9c526237e1f0dedaea
msp <msp@themsp.org>
2024-01-10 20:28:45 -0800

    update

--- modules/home-manager/neovim/config/lua/overseer.lua
+++ modules/home-manager/neovim/config/lua/overseer.lua
@@ -1,1 +1,3 @@
 require('overseer').setup()
+vim.keymap.set('n', '<Leader>rt', '<cmd>:OverseerRun<CR>', { desc = 'Run Tasks' })
+vim.keymap.set('n', '<Leader>rT', '<cmd>:OverseerToggle<CR>', { desc = 'Toggle Tasks' })
